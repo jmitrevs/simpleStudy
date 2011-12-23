941d2da358aba36fdd8b879f825602eb6a23217f
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2011-12-23 16:53:54 +0000

    fix code to test isEM selector

--- simpleStudy/TestAlg.h
+++ simpleStudy/TestAlg.h
@@ -69,7 +69,7 @@ private:
   /// a handle on Store Gate 
   StoreGateSvc* m_storeGate;
 
-  //ToolHandle<AthElectronIsEMSelector> m_electronSelector;
+  ToolHandle<AthElectronIsEMSelector> m_electronSelector;
   //ToolHandle<AthSelectorToolBase> m_electronSelector;
 
   //ToolHandle<TruthUtils> m_TruthUtils;
