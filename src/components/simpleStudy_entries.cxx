0a03e9daba86c1b69b2b6b11c97089df2045d9cb
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2016-10-10 15:21:19 +0000

    remove TruthTitls

--- src/components/simpleStudy_entries.cxx
+++ src/components/simpleStudy_entries.cxx
@@ -1,12 +1,9 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "simpleStudy/TestAlg.h"
-#include "simpleStudy/TruthUtils.h"
 
 DECLARE_ALGORITHM_FACTORY( TestAlg )
-DECLARE_TOOL_FACTORY( TruthUtils )
 
 DECLARE_FACTORY_ENTRIES( simpleStudy )
 {
 	DECLARE_ALGORITHM( TestAlg )
-	  DECLARE_TOOL( TruthUtils )
 }
